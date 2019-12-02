28494cf8dc1c955eb2fa9cd99e748e888239e8b4
Kenneth Webb <kenneth.webb@aplaceformom.com>
2019-12-02 13:37:25 -0600

    Adding Express API for the Workshop

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# git101
+# Git CLI 101
 Workshop training on Git Fundamentals
+
+# Pre-requisites
+- Install node.js on your machine (https://treehouse.github.io/installation-guides/mac/node-mac.html)
+- Install Git CLI (https://git-scm.com/download/mac)
+- Install Express Globally (npm install -g express-generator)
+
+# How to run?
+On the command line execute: npm start
+
+# How to test it?
+In a browser go to http://localhost:3000/teammates
---
+++ index.js
@@ -0,0 +1,34 @@
+var express = require("express");
+var app = express();
+
+app.get("/teammates", (req, res, next) => {
+    res.json([
+        "Adrian"
+        ,"Alejandro"
+        ,"Alexander P"
+        ,"Alexander R"
+        ,"Ana Beatriz"
+        ,"Andres"
+        ,"Brandon"
+        ,"Christian"
+        ,"Ernesto"
+        ,"Gia"
+        ,"Jean Carlo"
+        ,"Jimmy"
+        ,"Jorge"
+        ,"Kenneth"
+        ,"Kristopher"
+        ,"Laura"
+        ,"Luis"
+        ,"Mike"
+        ,"Olman"
+        ,"Rhedil"
+        ,"Rob"
+        ,"Rolo"
+        ,"Royner"
+    ]);
+});
+
+app.listen(3000, () => {
+    console.log("Workshop Server running on Port: 3000");
+});
