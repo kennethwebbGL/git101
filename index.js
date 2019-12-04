b6755f9af1abbc69281e5b16c712e785b88a177b
Alexander Rojas <alexander.rojas@adia.com>
2019-12-04 14:41:04 -0600

    Alex Pato changes

--- index.js
+++ index.js
@@ -29,7 +29,6 @@ app.get("/teammates", (req, res, next) => {
     ]);
 });
 
-<<<<<<< HEAD
 app.get("/pato", (req, res, next) => {
     res.json([
         "pato"
@@ -58,21 +57,51 @@ app.get("/pato", (req, res, next) => {
     ]);
 });
 
-app.get("/kenneth", (req, res, next) => {
-    res.json("name:Kenneth"
-    );
-});
-
-
-=======
 app.get("/glrooms", (req, res, next) => {
     res.json(["C#"
     ,"JS"
     ,"C++"
 ]);
 });
+app.get("/kenneth", (req, res, next) => {
+    res.json("name:Kenneth"
+    );
+});
 
->>>>>>> Adding rooms method
+app.get("/pato", (req, res, next) => {
+    res.json([
+        "pato"
+        ,"pato"
+        ,"pato P"
+        ,"pato R"
+        ,"pato Beatriz"
+        ,"pato"
+        ,"pato"
+        ,"pato"
+        ,"pato"
+        ,"pato"
+        ,"pato Carlo"
+        ,"pato"
+        ,"pato"
+        ,"pato"
+        ,"pato"
+        ,"pato"
+        ,"pato"
+        ,"pato"
+        ,"pato"
+        ,"pato"
+        ,"pato"
+        ,"Rolo"
+        ,"pato"
+    ]);
+});
+
+app.get("/glrooms", (req, res, next) => {
+    res.json(["C#"
+    ,"JS"
+    ,"C++"
+]);
+});
 app.listen(3000, () => {
     console.log("Workshop Adia Server running on Port: 3000");
 });
