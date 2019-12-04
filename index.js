313d18d58facc96642d21fd50dddf4e7838884b6
Luis Ruiz <luis.ruiz@gorillalogic.com>
2019-12-04 14:39:22 -0600

    Adding LuisR method

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ app.get("/teammates", (req, res, next) => {
 });
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 app.get("/glrooms", (req, res, next) => {
     res.json(["C#"
     ,"JS"
@@ -45,6 +46,14 @@ app.get("/kenneth", (req, res, next) => {
 
 
 >>>>>>> 77c8993e6bad092b33993cdcc3b521c660746798
+=======
+app.get("/luis", (req, res, next) => {
+    res.json([
+        "name:LuisR"
+    ]);
+});
+
+>>>>>>> Adding LuisR method
 app.listen(3000, () => {
     console.log("Workshop Adia Server running on Port: 3000");
 });
