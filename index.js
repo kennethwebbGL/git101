364d978cb0900d1c75beb32a4897e1e1a407abdf
Ernesto Casas Castro <ernesto.casas@Ernesto-Casas-MacBook-Pro.local>
2019-12-04 14:39:02 -0600

    Adding casas method

--- index.js
+++ index.js
@@ -29,6 +29,35 @@ app.get("/teammates", (req, res, next) => {
     ]);
 });
 
+app.get("/test", (req, res, next) => {
+    res.json([
+        "Adrian"
+        ,"Alejandro"
+        ,"Alexander P"
+        ,"Alexander R"
+        ,"Ana Beatriz"
+        ,"Andres"
+        ,"Brandon"
+        ,"Christian"
+        ,"Ernesto"
+        ,"Gia"
+        ,"Jean Carlo"
+        ,"Jimmy"
+        ,"Jorge"
+        ,"Kenneth"
+        ,"Kristopher"
+        ,"Laura"
+        ,"Luis"
+        ,"Mike"
+        ,"Olman"
+        ,"Rhedil"
+        ,"Rob"
+        ,"Rolo"
+        ,"Royner"
+    ]);
+});
+
+
 app.listen(3000, () => {
     console.log("Workshop Server running on Port: 3000");
 });
