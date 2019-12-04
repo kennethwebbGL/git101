9176e90da77120f919ae97585750cf0a555db7de
Ernesto Casas Castro <ernesto.casas@Ernesto-Casas-MacBook-Pro.local>
2019-12-04 15:11:59 -0600

    rebase chozas

--- index.js
+++ index.js
@@ -29,7 +29,6 @@ app.get("/teammates", (req, res, next) => {
     ]);
 });
 
-<<<<<<< HEAD
 app.get("/glrooms", (req, res, next) => {
     res.json(["C#"
     ,"JS"
@@ -37,14 +36,40 @@ app.get("/glrooms", (req, res, next) => {
 ]);
 });
 
-=======
 app.get("/kenneth", (req, res, next) => {
     res.json("name:Kenneth"
     );
 });
 
+app.get("/test", (req, res, next) => {
+    res.json([
+        "Adrian"
+        ,"Alejandro"
+        ,"Alexander P"
+        ,"Alexander R"
+        ,"Ana Beatriz"
+        ,"Andres"
+        ,"Brandon"
+        ,"Christian"
+        ,"Ernesto"
+        ,"Gia"
+        ,"Jean Carlo"
+        ,"Jimmy"
+        ,"Jorge"
+        ,"Kenneth"
+        ,"Kristopher"
+        ,"Laura"
+        ,"Luis"
+        ,"Mike"
+        ,"Olman"
+        ,"Rhedil"
+        ,"Rob"
+        ,"Rolo"
+        ,"Royner"
+    ]);
+});
 
->>>>>>> 77c8993e6bad092b33993cdcc3b521c660746798
+
 app.listen(3000, () => {
     console.log("Workshop Adia Server running on Port: 3000");
 });
